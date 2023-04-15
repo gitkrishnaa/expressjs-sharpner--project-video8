9572c287ea2e43d2eb441e1a3fec201bf4a419bd
krishna kumar <kri821084@gmail.com>
2023-04-15 10:45:34 +0530

    Initial commit

---
+++ controllers/shop.js
@@ -0,0 +1,53 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+exports.getProduct=(req,res,next)=>{
+const product_id=req.params.productId;
+Product.findById(product_id,(data)=>{
+  console.log(data)
+
+res.render('shop/product-detail',{
+  productKey:data,
+  pageTitle:data.title,
+  path:'/products',
+})
+})
+}
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
